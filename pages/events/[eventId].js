246a5e183758e3521be6862691fe4b53578ace4d
Nikola Vrhovac <vrllee@gmail.com>
2021-08-15 22:57:39 +0200

    finish app with mock data

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,36 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
+
 export default function EventDetailPage() {
+	const router = useRouter();
+
+	const eventId = router.query.eventId;
+	const event = getEventById(eventId);
+
+	if (!event) {
 		return (
-			<div>
-				<h1>Event Details</h1>
-			</div>
-		)
-};
+			<ErrorAlert>
+				<p>No event found!</p>
+			</ErrorAlert>
+		);
+	}
+
+	return (
+		<>
+			<EventSummary title={event.title} />
+			<EventLogistics
+				date={event.date}
+				address={event.location}
+				image={event.image}
+				imageAlt={event.title}
+			/>
+			<EventContent>
+				<p>{event.description}</p>
+			</EventContent>
+		</>
+	);
+}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,21 @@
+import { getAllEvents } from '../../dummy-data';
+import { useRouter } from 'next/router';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+
 export default function EventsPage() {
-		return (
-			<div>
-				<h1>Events Page - All Events</h1>
-			</div>
-		)
-};
+	const router = useRouter();
+	const events = getAllEvents();
+
+	function findEventsHandler(year, month) {
+		const fullPath = `/events/${year}/${month}`;
+		router.push(fullPath);
+	}
+
+	return (
+		<>
+			<EventsSearch onSearch={findEventsHandler} />
+			<EventList items={events} />
+		</>
+	);
+}
