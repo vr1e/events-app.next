f6ced234e61b652054e794b41b52bc4011653c6f
Nikola Vrhovac <vrllee@gmail.com>
2021-08-22 13:13:03 +0200

    setup newsletter and comments

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import EventContent from '../../components/event-detail/event-content';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventSummary from '../../components/event-detail/event-summary';
+import Comments from '../../components/input/comments';
 // import ErrorAlert from '../../components/ui/error-alert';
 import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
 
@@ -35,6 +36,7 @@ export default function EventDetailPage(props) {
 			<EventContent>
 				<p>{event.description}</p>
 			</EventContent>
+			<Comments eventId={event.id} />
 		</>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 import { getFeaturedEvents } from '../helpers/api-util';
 
 export default function HomePage(props) {
@@ -16,6 +17,7 @@ export default function HomePage(props) {
 					content='Find a lot of great events that allow you to improve...'
 				/>
 			</Head>
+			<NewsletterRegistration />
 			<EventList items={props.events} />
 		</div>
 	);
